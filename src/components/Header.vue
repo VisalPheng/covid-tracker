73308d939f418e4f27bc0e725e38b797421a14cd
VisalPheng <visal.pheng@student.niptict.edu.kh>
2021-10-02 02:11:44 +0700

    Initial

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="text-center bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold"><i class='bx bxs-virus'></i> Covid-19 Tracker</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
